869324b0ef1b5ee4aeae4d1efb1c5126e751f58d
Kaushal7 <125178199+kaushaltmk@users.noreply.github.com>
2023-09-06 10:09:15 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,60 @@
+"use client"
+import React, { useState } from 'react'
+
+const page = () => {
+  const [title, settitle] = useState("");
+  const [desc, setdesc] = useState("");
+  const [mainTask, setmainTask] = useState([]);
+  const submitHandler = (e) =>{
+    e.preventDefault();
+    setmainTask([...mainTask, {title, desc}]);
+    settitle("")
+    setdesc("")
+    console.log(mainTask)
+  }
+
+  const deleteHandler = (i) =>{
+    let copytask=[...mainTask]
+    copytask.splice(i,1);
+    setmainTask(copytask)
+  }
+
+  let renderTask = <h2>No task available</h2>
+
+  if(mainTask.length>0){
+    renderTask = mainTask.map((t, i)=>{
+      return(
+        <li key={i} className='flex items-center justify-between mb-5'>
+          <div className="flex justify-between items-center  w-2/3">
+          <h5 className='text-2xl font-semibold'>{t.title}</h5>
+          <h6 className='text-lg font-semibold'>{t.desc}</h6>
+          </div>
+          <button onClick={()=>{
+            deleteHandler(i)
+          }} className='bg-red-500 text-white rounded font-bold'>
+            Delete
+          </button>
+        </li>
+      )
+    })
+  }
+  return (
+<>
+    <h1 className='bg-black text-white p-5 font-bold text-xl' >Kaushal Todolist</h1>
+    <form onSubmit={submitHandler}>
+      <input className='text-2xl border-zinc-800 px-4 py-2 border-4 m-2' type="text" placeholder="Enter task here" value={title} onChange={(e)=>{
+        settitle(e.target.value)
+      }}/>
+      <input className='text-2xl border-zinc-800 px-4 py-2 border-4 m-2' type="text" placeholder="Enter description here" value={desc} onChange={(e)=>{
+        setdesc(e.target.value)
+      }}/>
+      <button className='bg-green-500 text-white px-4 py-2 text-2xl font-bold rounded m-5'>Add task</button>
+    </form>
+    <hr/>
+    <div className='p-8 bg-slate-200'>
+      <ul>{renderTask}</ul>
+    </div>
+</>
+  )
+}
+export default page
